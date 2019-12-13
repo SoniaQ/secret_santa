0b16ae8aa8a2d673bcdefde36be1a3c192042f7a
Sonia Quintiao <soniaquintiao@gmail.com>
2019-12-13 23:50:55 +0000

    Create second feature to test a user can see a form.

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -6,4 +6,11 @@ feature 'homepage' do
     expect(page).to have_content 'Organise your own Secret Santa online.'
   end
 
+  scenario 'a user can see the Secret Santa form and submit button' do
+    visit '/'
+    expect(page).to have_field 'Name'
+    expect(page).to have_field 'Email'
+    expect(page).to have_selector(:link_or_button, 'Submit')
+  end
+
 end
