c07c2645972b78f00e7fa0605248391b6bc7e77c
Sonia Quintiao <soniaquintiao@gmail.com>
2019-12-14 00:06:06 +0000

    Create third feature, testing a user can fill in form and submit a post request.

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -13,4 +13,15 @@ feature 'homepage' do
     expect(page).to have_selector(:link_or_button, 'Submit')
   end
 
+  scenario 'a user can submit name and email' do
+    visit '/'
+    fill_in(:name, with: 'Sonia')
+    fill_in(:email, with: 'sonia@random.com')
+    fill_in(:names, with: 'Daniel')
+    fill_in(:emails, with: 'daniel@random.com')
+    click_button('Submit')
+    expect(page).to have_content('Sonia, sonia@random.com')
+    expect(page).to have_content('Daniel, daniel@random.com')
+  end
+
 end
