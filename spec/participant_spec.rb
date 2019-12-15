9ee6ecd96c5cd88e71fd0143a3cf756825d2f6f0
Sonia Quintiao <soniaquintiao@gmail.com>
2019-12-15 18:17:41 +0000

    Create failing unit test to check participant email is returned. (Fourth feature test still needs to be fixed.)

--- spec/participant_spec.rb
+++ spec/participant_spec.rb
@@ -2,10 +2,16 @@ require 'participant'
 
 describe Participant do
   subject(:sonia) { Participant.new('Sonia') }
+  subject(:'sonia@random.com') { Participant.new('sonia@random.com') }
 
   describe '#name' do
-    it 'returns the name' do
+    it 'returns the participant name' do
       expect(sonia.name).to eq 'Sonia'
     end
+  describe '#emal' do
+    it 'returns the participant email' do
+      expect(sonia.email).to eq 'sonia@random.com'
+    end
+  end
   end
 end
