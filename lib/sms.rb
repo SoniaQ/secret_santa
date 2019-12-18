ce8cd19fe3e2f12e8950dd7a2d450d480b93747a
Sonia Quintiao <soniaquintiao@gmail.com>
2019-12-18 15:35:39 +0000

    Update sms.rb to include code that creates a new instance of the Message resource. Download ngrok and include in project route directory. Connect to ngrok and obtain our public url. Public url successfully displays our user page.

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,6 +1,21 @@
-class SMS
+require 'twilio-ruby'
 
-  def deliver
-  end
-  
-end
+account_sid = 'set environment variable'
+auth_token = 'set environment variable'
+client = Twilio::REST::Client.new(account_sid, auth_token)
+
+from = 'set environment variable' # Your Twilio number
+to = 'set environment variable' # Your mobile phone number
+
+client.messages.create(
+from: from,
+to: to,
+body: "Hey friend!"
+)
+
+# class SMS
+#
+#   def deliver
+#   end
+#
+# end
